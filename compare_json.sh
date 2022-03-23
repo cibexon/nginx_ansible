9ba20c7db8e53671c6f8e73e8c174e3af7673c2d
cibexon <cibexon@gmail.com>
2022-03-23 17:38:39 +0200

    updates

--- compare_json.sh
+++ compare_json.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 ansible-playbook compare_json.yml &&
+ echo  "   ##################################  Show difference between files ##################################     "
 cat output.txt
 rm -rf output.txt root_component.json params.json 
